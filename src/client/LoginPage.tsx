c1e1cc5e4b27553f74b502830fedac8df535c4db
Sergey Mishin <serg@pochtus.ru>
2026-02-18 20:05:26 +0300

    Use `react-hook-form` for forms

--- src/client/LoginPage.tsx
+++ src/client/LoginPage.tsx
@@ -6,24 +6,58 @@ import {
   Card,
   FormItem,
   FormLayoutGroup,
+  FormStatus,
   Input,
   Panel,
   PanelHeader,
 } from '@vkontakte/vkui'
+import { useRouter } from 'next/navigation'
 import * as React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { authClient } from '@/lib/auth/client'
 
 export function LoginPage() {
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [error, setError] = React.useState<string | null>(null)
+  interface Inputs {
+    email: string
+    password: string
+  }
 
-  const login = async () => {
-    setError(null)
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    setError,
+  } = useForm<Inputs>()
 
-    const { data, error } = await authClient.signIn.email({ email, password })
-    if (error) setError(error.message || 'Ошибка авторизации')
+  const [loading, setLoading] = React.useState(false)
+  const router = useRouter()
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    setLoading(true)
+
+    const { error } = await authClient.signIn.email({
+      email,
+      password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/profile')
+        },
+      },
+    })
+
+    if (error) {
+      switch (error.code) {
+        case 'INVALID_EMAIL_OR_PASSWORD':
+          setError('email', { message: error.message })
+          setError('password', { message: error.message })
+          break
+        default:
+          setError('root', { message: error.message || 'Ошибка авторизации' })
+      }
+    }
+
+    setLoading(false)
   }
 
   return (
@@ -31,41 +65,36 @@ export function LoginPage() {
       <PanelHeader>Авторизация</PanelHeader>
       <Card>
         <Box padding="m">
-          <FormLayoutGroup>
-            <FormItem
-              htmlFor="email"
-              top="E-mail"
-              required
-              status={error ? 'error' : 'default'}
-              bottom={error}
-            >
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormItem>
-            <FormItem
-              htmlFor="password"
-              top="Пароль"
-              required
-              status={error ? 'error' : 'default'}
-              bottom={error}
-            >
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormItem>
-            <FormItem>
-              <Button size="m" stretched onClick={login}>
-                Войти
-              </Button>
-            </FormItem>
-          </FormLayoutGroup>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormLayoutGroup>
+              <FormItem
+                htmlFor="email"
+                top="E-mail"
+                required
+                {...(errors.email && { status: 'error', bottom: errors.email.message })}
+              >
+                <Input id="email" type="email" {...register('email', { required: true })} />
+              </FormItem>
+              <FormItem
+                htmlFor="password"
+                top="Пароль"
+                required
+                {...(errors.password && { status: 'error', bottom: errors.password.message })}
+              >
+                <Input
+                  id="password"
+                  type="password"
+                  {...register('password', { required: true })}
+                />
+              </FormItem>
+              <FormItem>
+                <Button type="submit" size="m" stretched loading={loading}>
+                  Войти
+                </Button>
+              </FormItem>
+              {errors.root && <FormStatus mode="error">{errors.root.message}</FormStatus>}
+            </FormLayoutGroup>
+          </form>
         </Box>
       </Card>
     </Panel>
--- src/client/RegisterPage.tsx
+++ src/client/RegisterPage.tsx
@@ -11,21 +11,62 @@ import {
   Panel,
   PanelHeader,
 } from '@vkontakte/vkui'
+import { useRouter } from 'next/navigation'
 import * as React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { authClient } from '@/lib/auth/client'
 
 export function RegisterPage() {
-  const [username, setUsername] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [error, setError] = React.useState<string | null>(null)
+  interface Inputs {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+  }
 
-  const register = async () => {
-    setError(null)
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    setError,
+  } = useForm<Inputs>()
 
-    const { data, error } = await authClient.signUp.email({ name: username, email, password })
-    if (error) setError(error.message || 'Ошибка регистрации')
+  const [loading, setLoading] = React.useState(false)
+  const router = useRouter()
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ firstName, lastName, email, password }) => {
+    setLoading(true)
+
+    const { error } = await authClient.signUp.email({
+      name: `${firstName} ${lastName}`,
+      email,
+      password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/profile')
+        },
+      },
+    })
+
+    if (error) {
+      switch (error.code) {
+        case 'INVALID_EMAIL':
+        case 'USER_ALREADY_EXISTS':
+        case 'USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL':
+          setError('email', { message: error.message })
+          break
+        case 'INVALID_PASSWORD':
+        case 'PASSWORD_TOO_SHORT':
+        case 'PASSWORD_TOO_LONG':
+          setError('password', { message: error.message })
+          break
+        default:
+          setError('root', { message: error.message || 'Ошибка регистрации' })
+      }
+    }
+
+    setLoading(false)
   }
 
   return (
@@ -33,33 +74,54 @@ export function RegisterPage() {
       <PanelHeader>Регистрация</PanelHeader>
       <Card>
         <Box padding="m">
-          <FormLayoutGroup>
-            {error && <FormStatus mode="error">{error}</FormStatus>}
-            <FormItem htmlFor="username" top="Имя пользователя" required>
-              <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </FormItem>
-            <FormItem htmlFor="email" top="Электронная почта" required>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormItem>
-            <FormItem htmlFor="password" top="Пароль" required>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormItem>
-            <FormItem>
-              <Button size="m" stretched onClick={register}>
-                Создать аккаунт
-              </Button>
-            </FormItem>
-          </FormLayoutGroup>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormLayoutGroup>
+              <FormLayoutGroup mode="horizontal">
+                <FormItem
+                  htmlFor="firstName"
+                  top="Имя"
+                  required
+                  {...(errors.firstName && { status: 'error', bottom: errors.firstName.message })}
+                >
+                  <Input id="firstName" {...register('firstName', { required: true })} />
+                </FormItem>
+                <FormItem
+                  htmlFor="lastName"
+                  top="Фамилия"
+                  required
+                  {...(errors.lastName && { status: 'error', bottom: errors.lastName.message })}
+                >
+                  <Input id="lastName" {...register('lastName', { required: true })} />
+                </FormItem>
+              </FormLayoutGroup>
+              <FormItem
+                htmlFor="email"
+                top="Электронная почта"
+                required
+                {...(errors.email && { status: 'error', bottom: errors.email.message })}
+              >
+                <Input id="email" type="email" {...register('email', { required: true })} />
+              </FormItem>
+              <FormItem
+                htmlFor="password"
+                top="Пароль"
+                required
+                {...(errors.password && { status: 'error', bottom: errors.password.message })}
+              >
+                <Input
+                  id="password"
+                  type="password"
+                  {...register('password', { required: true })}
+                />
+              </FormItem>
+              <FormItem>
+                <Button type="submit" size="m" stretched loading={loading}>
+                  Создать аккаунт
+                </Button>
+              </FormItem>
+              {errors.root && <FormStatus mode="error">{errors.root.message}</FormStatus>}
+            </FormLayoutGroup>
+          </form>
         </Box>
       </Card>
     </Panel>
